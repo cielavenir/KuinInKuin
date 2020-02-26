686454e4a61d2e6a6250d5bb35e04b5d7933b359
cielavenir <cielartisan@gmail.com>
2020-02-27 01:12:18 +0900

    update

--- math_modMul.cpp
+++
@@ -1,4 +0,0 @@
-if(\{a_.id}<0)THROW(EXCPT_DBG_ARG_OUT_DOMAIN);
-if(\{b_.id}<0)THROW(EXCPT_DBG_ARG_OUT_DOMAIN);
-if(\{modulus_.id}<0)THROW(EXCPT_DBG_ARG_OUT_DOMAIN);
-return (int64_t)ModMul((uint64_t)\{a_.id}, (uint64_t)\{b_.id}, (uint64_t)\{modulus_.id});
